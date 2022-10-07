0d819df0505546e9782b9d78c3d2f65375efeb24
Jeremy Duncan <jduncan46@my.devry.edu>
2022-10-07 17:58:57 -0400

    Completed challenge 3 test

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -24,19 +24,21 @@
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // a)🧪 Create a test with expect statements using the variables provided.
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-it("returns a coded message", () => {
-  //=== Provided Test Cases ===
-  const secretCodeWord1 = "Lackadaisical";
-  const secretCodeWord2 = "Gobbledygook";
-  const secretCodeWord3 = "Eccentric";
-  //=== Expected Test Results ===
-  expected1 = "L4ck4d41s1c4l";
-  expected2 = "G0bbl3dyg00k";
-  expected3 = "3cc3ntr1c";
+describe("codeMessage", () => {
+  it("returns a coded message", () => {
+    //=== Provided Test Cases ===
+    const secretCodeWord1 = "Lackadaisical";
+    const secretCodeWord2 = "Gobbledygook";
+    const secretCodeWord3 = "Eccentric";
+    //=== Expected Test Results ===
+    expected1 = "L4ck4d41s1c4l";
+    expected2 = "G0bbl3dyg00k";
+    expected3 = "3cc3ntr1c";
 
-  expect(codeMessage(secretCodeWord1)).toEqual(expected1);
-  expect(codeMessage(secretCodeWord2)).toEqual(expected2);
-  expect(codeMessage(secretCodeWord3)).toEqual(expected3);
+    expect(codeMessage(secretCodeWord1)).toEqual(expected1);
+    expect(codeMessage(secretCodeWord2)).toEqual(expected2);
+    expect(codeMessage(secretCodeWord3)).toEqual(expected3);
+  });
 });
 
 //---------------------||❌ Initial Test Results ❌||---------------------------
@@ -109,26 +111,27 @@ const codeMessage = (msg) => {
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // a)🧪 Create a test with expect statements using the variables provided.
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-it("returns an array of all the words containing a particular letter", () => {
-  //=== Provided Test Cases ===
-  const fruitArray = [
-    "Mango",
-    "Cherry",
-    "Apricot",
-    "Blueberry",
-    "Peach",
-    "Kiwi",
-  ];
-  const letterA = "a";
-  const letterE = "e";
-  //=== Expected Test Results ===
-  expected1 = ["Mango", "Apricot", "Peach"];
-  expected2 = ["Cherry", "Blueberry", "Peach"];
+describe("getWordsThatContainLetter", () => {
+  it("returns an array of all the words containing a particular letter", () => {
+    //=== Provided Test Cases ===
+    const fruitArray = [
+      "Mango",
+      "Cherry",
+      "Apricot",
+      "Blueberry",
+      "Peach",
+      "Kiwi",
+    ];
+    const letterA = "a";
+    const letterE = "e";
+    //=== Expected Test Results ===
+    expected1 = ["Mango", "Apricot", "Peach"];
+    expected2 = ["Cherry", "Blueberry", "Peach"];
 
-  expect(getWordsThatContainLetter(fruitArray, letterA)).toEqual(expected1);
-  expect(getWordsThatContainLetter(fruitArray, letterE)).toEqual(expected2);
+    expect(getWordsThatContainLetter(fruitArray, letterA)).toEqual(expected1);
+    expect(getWordsThatContainLetter(fruitArray, letterE)).toEqual(expected2);
+  });
 });
-
 //---------------------||❌ Initial Test Results ❌||---------------------------
 // FAIL  ./code-challenges.test.js
 // ✓ returns a coded message (2 ms)
@@ -183,7 +186,71 @@ const getWordsThatContainLetter = (arr, ltr) => {
 // and determines whether or not the array is a "full house". A full house is
 // exactly one pair and one three of a kind.
 //******************************************************************************
+describe("getWordsThatContainLetter", () => {
+  it("determines whether or not the array is a “full house”", () => {
+    //=== Provided Test Cases ===
+    const hand1 = [5, 5, 5, 3, 3];
+    const hand2 = [5, 5, 3, 3, 4];
+    const hand3 = [5, 5, 5, 5, 4];
+    const hand4 = [7, 2, 7, 2, 7];
+    //=== Edge Case ===
+    const hand5 = ["King", "King", "Ace", "Ace", "Ace"];
+    const hand6 = ["Jack", "Queen", "Ace", "Ace", "Ace"];
+    const hand7 = ["King", "King", 4, 4, 4];
+    const hand8 = [1, 1, "Ace", "Ace", "Ace"];
 
+    //=== Expected Test Results ===
+    expected1 = true;
+    expected2 = false;
+    expected3 = false;
+    expected4 = true;
+    //=== Edge Cases ===
+    expected5 = true;
+    expected6 = false;
+    expected7 = true;
+    expected8 = true;
+
+    expect(determineFullHouse(hand1)).toEqual(expected1);
+    expect(determineFullHouse(hand2)).toEqual(expected2);
+    expect(determineFullHouse(hand3)).toEqual(expected3);
+    expect(determineFullHouse(hand4)).toEqual(expected4);
+
+    expect(determineFullHouse(hand5)).toEqual(expected5);
+    expect(determineFullHouse(hand6)).toEqual(expected6);
+    expect(determineFullHouse(hand7)).toEqual(expected7);
+    expect(determineFullHouse(hand8)).toEqual(expected8);
+  });
+});
+
+//---------------------||❌ Initial Test Results ❌||---------------------------
+// FAIL  ./code-challenges.test.js
+// codeMessage
+//   ✓ returns a coded message (1 ms)
+// getWordsThatContainLetter
+//   ✓ returns an array of all the words containing a particular letter
+//   ✕ determines whether or not the array is a “full house” (1 ms)
+
+// ● getWordsThatContainLetter › determines whether or not the array is a “full house”
+
+//   ReferenceError: determineFullHouse is not defined
+
+//     211 |     expected8 = true;
+//     212 |
+//   > 213 |     expect(determineFullHouse(hand1)).toEqual(expected1);
+//         |     ^
+//     214 |     expect(determineFullHouse(hand2)).toEqual(expected2);
+//     215 |     expect(determineFullHouse(hand3)).toEqual(expected3);
+//     216 |     expect(determineFullHouse(hand4)).toEqual(expected4);
+
+//     at Object.expect (code-challenges.test.js:213:5)
+
+// Test Suites: 1 failed, 1 total
+// Tests:       1 failed, 2 passed, 3 total
+// Snapshots:   0 total
+// Time:        0.217 s, estimated 1 s
+// Ran all test suites.
+// error Command failed with exit code 1.
+//------------------------------------------------------------------------------
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // a)🧪 Create a test with expect statements using the variables provided.
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
